70f066e8c758666cdb85b2e74215ad490ee577a5
Raphael Jay <iteachmyselfwebdev@gmail.com>
2022-09-18 00:39:47 +0800

    colors added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import products from './data/products.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
